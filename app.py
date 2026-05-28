32b5fa216663a623547a3cc1190d7a875852ffd5
Anderson Hamilton <anderson.andrew.hamilton@gmail.com>
2026-05-28 11:37:58 +0700

    Final

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="British English Learning Center",
+    page_title="Vocab",
     page_icon="🇬🇧",
     layout="centered"
 )
